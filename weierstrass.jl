e31dd7096df1ff3d8e832a97b53ec35a20c869e2
Pedro Cavalcante Oliveira <pedrocolrj@gmail.com>
2019-08-04 18:52:07 -0300

    Create weierstrass.jl

---
+++ weierstrass.jl
@@ -0,0 +1,12 @@
+### Pedro Cavalcante
+### pc_oliveira@id.uff.br
+
+### Implementação da Função de Weierstrass
+### Tópicos Especiais de Métodos Estatísticos
+
+
+# Primeiro definimos uma função de teste de par e ímpar
+
+function is_par(n)
+
+  
